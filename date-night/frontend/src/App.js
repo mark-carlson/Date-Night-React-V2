57bd13258610853e781c31b7ba2d24a1de83c21e
Jennifer Stokes <jenniferstokes@airbears2-10-142-153-119.airbears2.1918.berkeley.edu>
2018-04-14 10:56:19 -0700

    fix merge conflict

--- date-night/frontend/src/App.js
+++ date-night/frontend/src/App.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Nav from "./Components/Nav";
-import Logon from "./Components/Logon"
+import Logon from "./Components/Logon";
 
 const App = () => (
   <div>
