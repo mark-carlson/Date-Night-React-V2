dc7b5363c963bc9d57c5e2f0d275e7606819849c
Louella Landicho <louellalandicho@calvisitor-10-105-172-245.calvisitor.1918.berkeley.edu>
2018-04-14 12:07:48 -0700

    commit dollardates

--- date-night/frontend/src/App.js
+++ date-night/frontend/src/App.js
@@ -2,13 +2,19 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Nav from "./Components/Nav";
-import Logon from "./Components/Logon"
+import Logon from "./Components/Logon";
+import DollarDates from "./Components/DollarDates/DollarDates";
+import HelloTest from "./Components/HelloTest/HelloTest"
 
 const App = () => (
   <div>
     <Nav />
     <Logon />
- 
+    <DollarDates />
+    <DollarDates />
+    <HelloTest />
   </div>
+
+  
 );
 export default App;
--- date-night/frontend/src/Components/DollarDates/DollarDates.js
+++ date-night/frontend/src/Components/DollarDates/DollarDates.js
@@ -1,27 +1,29 @@
-import React from 'react'
+import React from "react";
+// import Button from "react-materialize";
+import { Button } from "react-materialize";
 
+const DollarDates = () => (
+    //Add selected items to search bar when they exist, input field collapses as selected items are added 
+    //onKeyUp used instead of onChange in order to capture delete/backspace key
 
-export const DollarDates = React.createClass({
-
-render () {
-    return (
-    <div className='dollar-date-container'>
-        <div className='title'>
-        <span> Title: goes here</span>
-            </div>
-
-        <div className='photo'>
-        <span> Photo: <img src='http://via.placeholder.com/200x200'></span>
+    <div className="dollar-date-container">
+        <div className="title">
+            <span> Title: goes here</span>
         </div>
 
-        <div className='description'>
+        <div className="photo">
+            <span> Photo: <img src="http://via.placeholder.com/200x200" /></span>
+        </div>
+
+        <div className="description">
             <span> Description: goes here</span>
-                </div>
-                <div className='cost'>
-                <span> Cost: goes here</span>
-                </div>
+        </div>
+        
+        <div className="cost">
+            <span> Cost: goes here</span>
+        </div>
+        <Button>hiiii</Button>
+    </div>
 
-    )
-    }
-
-    })
+  );
+  export default DollarDates
