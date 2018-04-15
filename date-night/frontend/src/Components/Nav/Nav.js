279ea5be6572d5114e93cd215a024a7c7491bd9f
Jennifer Stokes <jenniferstokes@Jennifers-MacBook-Pro.local>
2018-04-15 12:47:52 -0700

    fixed logon merge erros

--- date-night/frontend/src/Components/Nav/Nav.js
+++ date-night/frontend/src/Components/Nav/Nav.js
@@ -1,32 +1,5 @@
 import React from "react";
 import "./Nav.css";
-<<<<<<< HEAD
-import  Logon  from "../Logon/Logon"
-import { logout } from "../../utils/storage";
-
-
-
-const Nav = () => (
-  <nav className="navbar navbar-inverse navbar-top">
-    <div className="container-fluid">
-      <div className="navbar-header">
-        <a href="/movienight" className="navbar-brand">
-          Dinner & Movie
-        </a>
-        <span className="navbar-brand">|</span>
-        <a href="/wheel" className="navbar-brand">
-          Spin the Wheel
-        </a>
-        <span className="navbar-brand">|</span>
-        <a href="/dollardates" className="navbar-brand">
-          Dollar Dates
-        </a>
-        <span className="navbar-brand">|</span>
-        <a href="/" onClick={Logon.logout} className="navbar-brand">
-          Sign Out
-        </a>
-      </div>
-=======
 import Logon from "../Logon/Logon.js"
 
 console.log(Logon.constructor())
@@ -41,7 +14,6 @@ const Nav = () => (
         <li><a href="/dollardates">Dollar Dates</a></li>
         <li><a href="/">Sign Out</a></li>
       </ul>
->>>>>>> 9167762f865013a697754404faa58c8e5624cb11
     </div>
   </nav>
 
