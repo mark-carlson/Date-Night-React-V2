3f24bc37f1456c45e37eee32274d0ede35aafc4e
zeelee28 <32179557+zeelee28@users.noreply.github.com>
2018-04-15 14:22:07 -0700

    got the spinner going

--- date-night/frontend/src/Components/Wheel.js
+++ date-night/frontend/src/Components/Wheel.js
@@ -1,14 +1,14 @@
 import React from 'react'
 import Nav from './Nav/Nav'
 import { Button, Card, Col, Input, Row } from "react-materialize"
+import Spinner from "./Spinner/Spinner"
 
 class Wheel extends React.Component{
     render(){
         return(
             <div className="wheel-page">
                 <Nav />
-                <h1>This is going to be the wheel page</h1>
-
+                <Spinner />
             </div>
         )
     }
