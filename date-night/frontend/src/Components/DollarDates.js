1795aa6cc21c1b7ec4be8876af871bd4f29d3dc9
Louella Landicho <louellalandicho@calvisitor-10-105-175-215.calvisitor.1918.berkeley.edu>
2018-04-17 19:23:07 -0700

    adding homepage

--- date-night/frontend/src/Components/DollarDates.js
+++ date-night/frontend/src/Components/DollarDates.js
@@ -1,15 +1,47 @@
 import React from 'react'
 import Nav from './Nav/Nav'
-import { Button, Card, Col, Input, Row } from "react-materialize"
+import { Button, Card, Col, Input, Row, CardTitle } from "react-materialize"
 
 class DollarDates extends React.Component{
     render(){
         return(
-            <div className="dollar-page">
+    <div className="dollar-page">
                 <Nav />
-                <h1>This is going to be the dollar dates page</h1>
 
-            </div>
+
+   
+        <div class="container">
+
+        <Card className='blue-grey darken-1' title='Bike Date'>
+
+        </Card>
+        
+                <Card className='small' header={<CardTitle image='/img/sunset1.jpg'>Bike Dates</CardTitle>} actions={[<a href='#'>This is a Link</a>]}>Lorem ipsum dolor sit amet, id fugit convenire iracundia quo. Affert nostro civibus cum ei. Vix accumsan delicata reprehendunt et, te convenire dissentiet mediocritatem has.
+                <p><strong>Cost:</strong> $ 0.00</p>
+        </Card>
+
+
+        <Card className='blue-grey darken-1' title='Bike Date'>
+
+        </Card>
+        
+                <Card className='small' header={<CardTitle image='/img/sunset2.jpg'>Bike Dates</CardTitle>} actions={[<a href='#'>This is a Link</a>]}>Lorem ipsum dolor sit amet, id fugit convenire iracundia quo. Affert nostro civibus cum ei. Vix accumsan delicata reprehendunt et, te convenire dissentiet mediocritatem has.
+                <p><strong>Cost:</strong> $ 0.00</p>
+        </Card>
+
+        <Card className='blue-grey darken-1' title='Bike Date'>
+
+        </Card>
+        
+                <Card className='small' header={<CardTitle image='/img/sunset3.jpg'>Bike Dates</CardTitle>} actions={[<a href='#'>This is a Link</a>]}>Lorem ipsum dolor sit amet, id fugit convenire iracundia quo. Affert nostro civibus cum ei. Vix accumsan delicata reprehendunt et, te convenire dissentiet mediocritatem has.
+                <p><strong>Cost:</strong> $ 0.00</p>
+        </Card>
+
+
+
+        </div>
+
+    </div>
         )
     }
 }
--- date-night/frontend/src/Components/HomePage.js
+++ date-night/frontend/src/Components/HomePage.js
@@ -1,16 +1,60 @@
-import React from 'react'
-import Nav from './Nav/Nav'
-import { Button, Card, Col, Input, Row } from "react-materialize"
+import React from 'react';
+import Nav from './Nav/Nav';
+import { Button, Card, Col, Input, Row, CardTitle, MediaBox, CardPanel, Dropdown, NavItem } from "react-materialize";
+import "../App.css";
 
 class HomePage extends React.Component{
     render(){
         return(
-            <div className="home-page">
+
+            
+                <div className="home-page">
                 <Nav />
-                <div>this is the homepage</div>
-            </div>
+
+<div className="mainBG">
+<div class="content" >
+
+
+    <div class="row">
+        <div class="row">
+            <div class="col s12 m4 l2"><p></p></div>
+            <div class="col s12 m4 l8"><p><h2>Find the Best Date Places in Town</h2></p></div>
+            <div class="col s12 m4 l2"><p></p></div>
+        </div>
+    </div>
+
+
+<div class="circles-parent">
+            
+    <div class="square"><img src="/img/moviedinner.jpg" style={{width: 250, height: 200}} /> 
+    <p>Dinner & Movie</p>
+    </div>
+            
+ 
+    <div class="square"><img src="/img/spinthewheel3.jpg" style={{width: 250, height: 200}} />
+    <p>Spin the Wheel</p>
+    </div>
+
+
+    <div class="square"><img src="/img/dollardates3.jpg" style={{width: 250, height: 200}} />
+    <p>Dollar Dates</p>
+    </div>
+
+ 
+    <div class="square"><img src="/img/saved.jpg" style={{width: 250, height: 200}} />
+    <p>Saved Dates</p>
+    </div>
+
+</div>
+
+
+</div> 
+</div>
+
+            </div>       
+            
         )
     }
 }
 
-export default HomePage
+export default HomePage;
